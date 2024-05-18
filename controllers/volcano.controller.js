8090bd6bf13aac3f654b2e58cf90d676af757661
Arfin Hayet <syed@primetechbd.com>
2024-05-18 23:36:13 +0600

    volcano and country api added

--- controllers/volcano.controller.js
+++ controllers/volcano.controller.js
@@ -4,33 +4,44 @@ const knexConfig = require('../knexfile').development;
 const db = knex(knexConfig);
 
 async function httpGetAllVolcanos(req, res, next) {
-  const users = await db('data').select('*');
+  const {country,populatedWithin} = req.query;
+  // Build the base query
+  let query = db('data').select('id', 'name', 'region', 'subregion')
+    .where('country', country);
 
-  const payload = {};
-  return sendResponse({
-    status: "success",
-    statusCode: 200,
-    message: "Get tests successfully!",
-    payload : users,
-    res,
-  });
+  // Add population conditions only if populatedWithin is provided
+  if (populatedWithin) {
+    query = query.andWhere(function() {
+      this.orWhere('population_5km', populatedWithin)
+        .orWhere('population_10km', populatedWithin)
+        .orWhere('population_30km', populatedWithin)
+        .orWhere('population_100km', populatedWithin);
+    });
+  }
+  const users = await query;
+  res.json(users);   
 }
 
 
 async function httpGetOneVolcano(req, res, next) {
   const { id } = req.params;
-  const users = await db('data').where('id', id).first();
-  const payload = {};
-  return sendResponse({
-    status: "success",
-    statusCode: 200,
-    message: "Get tests successfully!",
-    payload : users,
-    res,
-  });
+  const users = await db('data').select('id','name','country','region','subregion','last_eruption','summit','elevation','latitude','longitude')
+  .where('id', id).first();
+  res.json(users);
+}
+
+
+
+async function httpGetCountry(req, res, next) {
+  const countries = await db('data')
+      .distinct('country')
+      .orderBy('country', 'asc'); // Order by country name in descending order
+  const countryList = countries.map(row => row.country);
+  res.json(countryList);
 }
 
 module.exports = {
   httpGetAllVolcanos,
-  httpGetOneVolcano
+  httpGetOneVolcano,
+  httpGetCountry
 };
--- routes/volcano.routes.js
+++ routes/volcano.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const catchAsync = require("../services/catchAsync");
 
-const { httpGetAllVolcanos, httpGetOneVolcano } = require("../controllers/volcano.controller");
+const { httpGetAllVolcanos, httpGetOneVolcano, httpGetCountry } = require("../controllers/volcano.controller");
 
 const router = Router();
 
 router.route("/volcanoes").get(catchAsync(httpGetAllVolcanos));
 router.route("/volcano/:id").get(catchAsync(httpGetOneVolcano));
+router.route("/countries").get(catchAsync(httpGetCountry));
 
 module.exports = router;
