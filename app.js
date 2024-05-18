2b12d624e38b590f4e8b479292ae0f57e2bea6d5
rummanH <jah.hasant@gmail.com>
2024-05-18 23:09:36 +0600

    added the me endpoint

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+// Core node Modules
+const fs = require("fs");
+const path = require("path");
+
+// 3rd Party libraries
+const swaggerUi = require("swagger-ui-express");
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+
+// Own modules
 const globalErrorHandler = require("./controllers/error.controller");
-const corsOptions = require("./services/corsOptions");
+const { getMe } = require("./controllers/profile.controller");
 const volcanoRouter = require("./routes/volcano.routes");
+const corsOptions = require("./services/corsOptions");
 const userRouter = require("./routes/user.router");
 const AppError = require("./services/AppError");
+
 const app = express();
 // app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(helmet());
 app.use(cors(corsOptions));
 // Read the Swagger JSON file
-const swaggerDocument = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'swagger.json'), 'utf-8'));
+const swaggerDocument = JSON.parse(fs.readFileSync(path.resolve(__dirname, "swagger.json"), "utf-8"));
 
-
-
-app.use("/", volcanoRouter);
-app.use("/user", userRouter);
-
-
-
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
+
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+// API Routes
+app.use("/", volcanoRouter);
+app.use("/user", userRouter);
+app.get("/me", getMe);
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.all("*", (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)));
 app.use(globalErrorHandler);
 
